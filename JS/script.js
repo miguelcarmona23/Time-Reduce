c431bc13422fdfe0932cd074aafc028d9607f7cd
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 16:46:41 -0500

    Updated const timeNodes to an array

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,3 @@
-const timeNodes = document.querySelectorAll('[data-time]');
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 
 //const seconds = timeNodes.map(node => node.dataset.time);
