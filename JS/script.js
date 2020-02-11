fe273114a42626211f4bee7ec773854ba1a9ea45
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 16:58:28 -0500

    Added const mins

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,8 @@ const seconds = timeNodes
 let secondsLeft = seconds;
 const hours = Math.floor(secondsLeft / 3600);
 secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(secondsLeft);
